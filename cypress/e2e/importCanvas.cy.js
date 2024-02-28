8f101e5d7583f11eab61e428f7782da7dee313a4
gttestlogin <gt.test.login@gmail.com>
2024-02-28 21:46:28 +0700

    Feb26 - importCanvas

--- cypress/e2e/importCanvas.cy.js
+++ cypress/e2e/importCanvas.cy.js
@@ -3,85 +3,155 @@ describe('template spec', () => {
     cy.loginByAPIgetRefreshToken('https://mathgpt-api-dev.tutoruniverse.net/token-refreshes', `eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI0NjQiLCJleHAiOjE3MDk1NTAyMjcsImp0aSI6ImI3MTgwMDg0ZDQ5NjExZWViN2U1YmVhOTQ1MTBkNzQ1IiwiaXNzIjoiZ290LWl0LmFpL3Bob3Rvc3R1ZHkifQ.bechdNejJbc9_EpVjJq6UoGxRVxXL-zeruh6wtsBoy13ICCZwyuhMdMNaB8RXbW0HWYtIveDMDvTz8xfpNUMAwzow_RKDRG41O4IyZcW2r9XzvBIX_Vh16Ztra72iz9_rMTYTSKT9Qzs5srr7-d71bJ6W70YG8EhJ5dt3xMVSsrroj5cVKcB0bJag-CXHDTmUqdV5NRwSJHNDMJHA02wHabYUGKVLd2OYvVY6sErFmM2mUlSEt-ozpfKBqpp_dsV0BkQ-pIRNB0i42hGinX2gBefK3HaLPGyahRm0hiOgaeyXJmGZPmIJYi9vCGcjZ0sGguFnrP1wA_RBjC0RFx09w`)
   })
 
-  it('importCanvas1', function () {
-    /* ==== Generated with Cypress Studio ====
-    cy.visit('https://dev.mathgpt.ai/login?role=educator');
-    cy.get(':nth-child(1) > .FormInput').clear().type('manh+edu1@gotitapp.co');
-    cy.get('.FormInputWrapper-sc-8v1liz-1 > .FormInput').clear().type('Aa123456@');
-    cy.get('.u-backgroundPrimary').click();
-    */
+  it.skip('importCanvas1', function () {
 
-    cy.visit('https://dev.mathgpt.ai/home')
-    //click on Create button
-    cy.contains('Create new course').click({ force: true })
-    cy.contains('Import from Canvas').click({ force: true })
-    // cy.get('.u-marginBottomMedium > .u-flex > .Button > .Button-label').click();
-    // cy.get('.u-gapSmall > :nth-child(1)').click();
-    
-    //click on "demo1" course
-    // cy.get(':nth-child(4) > .u-zIndex2 > .u-flexGrow1 > .u-justifyContentBetween > .Button > .u-fontSemiBold').click();
-    //selector "Import the course" list: [class^=CourseItemContentWrapper] + div > button
-    // cy.get('[class^=CourseItemContentWrapper] > div:first-child').each(($element) => {
-    //   const text = $element.text().trim();
-    //   if (text.includes('demo1')) {
-    //     // Find the button in the sibling div
-    //     cy.get($element).siblings('div').find('button').click();
-    //   }
-    // });
+    const demoValues = ['demo1', 'demo2', 'demo3'];
 
-    /*
-    cy.get('[class^=CourseItemContentWrapper] > div:first-child').each(($element) => {
-      const text = $element.text().trim();
-      if (text.includes('demo1')) {
-        // Find the sibling div and then find the button within it
-        cy.get('[class^=CourseItemContentWrapper]').siblings('div').find('button').click();
-      }
-    });
-    */
-    cy.get('[class^=CourseItemWrapper]').each(($parentElement) => {
-      // Check if the specific CSS selector contains 'demo1' text
-      if ($parentElement.find('> div:last-child > div > div:first-child > div:first-child').text().includes('demo1')) {
-        // Click on the button within the specific CSS selector
-        $parentElement.find('> div:last-child > div > div:last-child > button').click();
-      }
-    });
+    for (const demoValue of demoValues) {
+      // Visit the URL and click on "Import from Canvas"
+      cy.visit('https://dev.mathgpt.ai/courses/new');
+      cy.contains('Import from Canvas').click({ force: true });
 
-    cy.get('#react-select-2-placeholder').click();
-    cy.get('#react-select-2-option-2').click();
-    // cy.get('[data-testid="date-picker__startDate__input"] > .u-flexGrow1').click();
-    // cy.get('.react-calendar__month-view__days > :nth-child(33)').click();
-    // cy.get('[data-testid="date-picker__endDate__input"] > .u-flexGrow1').click();
-    // cy.get('.react-calendar__navigation__next-button').click();
-    // cy.get('.react-calendar__month-view__days > :nth-child(35)').click();
-    
-    //import student
-    cy.get('.FormCheck').click();
+      // Click on the button for the current demoValue
+      cy.get('[class^=CourseItemWrapper]').each(($parentElement) => {
+        const specificElement = $parentElement.find('> div:last-child > div > div:first-child > div:first-child');
 
-    //select Algebra book
-    cy.contains('College Algebra 2e').click()
-    // cy.get(':nth-child(1) > .u-justifyContentCenter > .BookThumbnailWrapper-sc-r3zu6m-0 > .u-zIndex2 > .u-maxWidthFull').click();
-    
-    cy.contains('Create course').last().click()
-    //cy.get('.u-backgroundPrimary').click();
-    // cy.get('.u-marginLeftMedium > .ut-textGray900').click();
-    // cy.get('.u-marginLeftMedium > .ut-textGray900').click();
-    // cy.get('.u-marginLeftMedium > .ut-textGray900').click();
-    cy.contains('Yay! Your course has been created.')
-    cy.contains('This course has been imported from Canvas.')
-    cy.contains('Last updated from Canvas:')
-    // cy.get('.u-marginLeftMedium > .ut-textGray900').should('have.text', 'Yay! Your course has been created.');
-    // cy.get('.u-marginLeftMedium > .ut-textGray900').should('have.text', 'Yay! Your course has been created.');
-    // cy.get('.u-flexGrow1 > :nth-child(1) > .u-marginBottomExtraSmall').should('have.text', 'This course has been imported from Canvas. You\'re unable to add, edit, or remove contents here. Please access Canvas to make any modifications to the course.');
-    // cy.get(':nth-child(4) > .u-flex > .u-flexGrow1 > .u-textWordBreak > .ReactMarkdownContainer-sc-uxrz4f-0 > .markdown-content > p').click();
-    // cy.get('[data-testid="heading"] > :nth-child(1)').click();
-    cy.contains('Students').click()
-    cy.contains('Student name')
-    // cy.get('[data-testid="heading"] > :nth-child(1)').click();
-    // cy.get('[data-testid="heading"] > :nth-child(1)').click();
-    // cy.get('[data-testid="heading"] > :nth-child(1)').should('have.text', 'Student name');
-    // cy.get(':nth-child(2) > .u-flex > .u-flexGrow1 > .u-textWordBreak > .ReactMarkdownContainer-sc-uxrz4f-0 > .markdown-content > p').click();
-    // cy.get(':nth-child(1) > .u-flex > .u-flexGrow1 > .u-textWordBreak > .ReactMarkdownContainer-sc-uxrz4f-0 > .markdown-content > p').click();
-    /* ==== End Cypress Studio ==== */
+        if (specificElement.text().includes(demoValue)) {
+          $parentElement.find('> div:last-child > div > div:last-child > button').click();
+        }
+      });
+      cy.wait(3000)
+
+      cy.get('#react-select-2-placeholder').click();
+      cy.get('#react-select-2-option-2').click();
+
+      //import student
+      cy.get('.FormCheck').click();
+
+      //select Algebra book
+      cy.contains('College Algebra 2e').click()
+
+      cy.contains('Create course').last().click()
+
+      cy.contains('Yay! Your course has been created.')
+      cy.contains('This course has been imported from Canvas.')
+      cy.contains('Last updated from Canvas:')
+
+      cy.contains('Students').click()
+      cy.contains('Student name')
+
+      cy.wait(3000)
+    }
+
   })
 
+  it.skip('create a Canvas account', function () {
+    cy.visit('https://canvas.tutoruniverse.net/enroll/XW974W');
+    cy.get('#student_email').clear().type('manh+demo3@gotitapp.co');
+    cy.wait(1000)
+    cy.contains('I am a new user').click()
+    cy.get('#student_name').type('manh+demo3@gotitapp.co');
+    cy.get('#submit_button').click();
+    //sign out
+    cy.get('img')
+    cy.contains('Logout')
+  })
+
+  it.skip('create 60 Canvas accounts - not really well structured', function () {
+    const accountsToCreate = 30;
+    const emailPrefix = 'manh+demo';
+    const baseEmail = '@gotitapp.co';
+
+    for (let i = 45; i <= accountsToCreate + 44; i++) {
+      const email = emailPrefix + i + baseEmail;
+
+      cy.visit('https://canvas.tutoruniverse.net/enroll/XW974W');
+      cy.wait(2000);
+      cy.get('#student_email').clear().type(email);
+      cy.wait(2000);
+      cy.contains('I am a new user').click();
+      cy.get('#student_name').type(email);
+      cy.wait(2000)
+      cy.get('#submit_button').click();
+      cy.wait(2000)
+      cy.visit('https://canvas.tutoruniverse.net/')
+      cy.wait(5000);
+
+      //sign out
+      cy.get('#global_nav_profile_link').click()
+      cy.log('email: ' + email)
+      cy.wait(2000);
+      cy.contains('Logout').click()
+      cy.wait(2000);
+      cy.visit('https://canvas.tutoruniverse.net/')
+      cy.wait(2000);
+
+    }
+  });
+
+  it.skip('use Canvas admin to add user/pass, remove old user, update new user email', function () {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('https://canvas.tutoruniverse.net/login/canvas');
+    cy.get('#pseudonym_session_unique_id').type('jon@gotitapp.co');
+    cy.get('#pseudonym_session_password').type('b1CtUXftRhF2');
+    cy.get(`input[value='Log In']`).click();
+
+    cy.visit('https://canvas.tutoruniverse.net/accounts/2/users');
+    cy.wait(1000)
+    cy.get('#TextInput_1').type('manh+demo2@gotitapp.co');
+    cy.wait(1000)
+    //click on expected email to change pass
+    cy.get(`a[data-cid='Link']`).click();
+    cy.wait(1000)
+    //Add Login
+    cy.get('.add_pseudonym_link').click();
+    //input new credentials
+    cy.get('#pseudonym_unique_id').type('manh+demo2a@gotitapp.co');
+    cy.get('#pseudonym_password').type('Aa123456@');
+    cy.get('#pseudonym_password_confirmation').type('Aa123456@');
+    //Click "Add Login" button
+    cy.get('#edit_pseudonym_form > .form-controls > .btn-primary').click();
+    //Delete the first Login information
+    cy.get(':nth-child(1) > .links > .delete_pseudonym_link > .icon-end').click();
+    //Edit the second Login information to become the first
+    cy.get('[style=""] > .links > .edit_pseudonym_link > .icon-edit').click();
+    cy.get('#pseudonym_unique_id').clear().type('manh+demo2@gotitapp.co');
+    //Update Log in
+    cy.get('#edit_pseudonym_form > .form-controls > .btn-primary').click();
+  })
+
+  it('use Canvas admin to add user/pass, remove old user, update new user email', function () {
+    cy.visit('https://canvas.tutoruniverse.net/login/canvas');
+    cy.get('#pseudonym_session_unique_id').type('jon@gotitapp.co');
+    cy.get('#pseudonym_session_password').type('b1CtUXftRhF2');
+    cy.get(`input[value='Log In']`).click();
+
+
+
+    for (let i = 61; i <= 65; i++) {
+
+      cy.visit('https://canvas.tutoruniverse.net/accounts/2/users');
+      cy.wait(1000);
+
+      const baseEmail = `manh+demo${i}`;
+      const email = `${baseEmail}@gotitapp.co`;
+      const emailWithA = `${baseEmail}a@gotitapp.co`;
+
+      cy.get('#TextInput_1').type(email);
+      cy.wait(2000);
+      cy.get(`a[data-cid='Link']`).click();
+      cy.wait(10000);
+      cy.get('.add_pseudonym_link').click();
+      cy.wait(2000);
+      cy.get('#pseudonym_unique_id').type(emailWithA);
+      cy.get('#pseudonym_password').type('Aa123456@');
+      cy.get('#pseudonym_password_confirmation').type('Aa123456@');
+      cy.get('#edit_pseudonym_form > .form-controls > .btn-primary').click();
+      cy.get(':nth-child(1) > .links > .delete_pseudonym_link > .icon-end').click();
+      cy.get('[style=""] > .links > .edit_pseudonym_link > .icon-edit').click();
+      cy.get('#pseudonym_unique_id').clear().type(email);
+      cy.get('#edit_pseudonym_form > .form-controls > .btn-primary').click();
+
+    }
+  });
 })
